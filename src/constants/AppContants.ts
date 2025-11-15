bed9658863f26c3a31a575638e6bc6fb426bb7a6
Marcos Antônio <marcoscardoso.programmer@gmail.com>
2025-11-14 22:11:10 -0300

    change API_URL

--- src/constants/AppContants.ts
+++ src/constants/AppContants.ts
@@ -1,3 +1,3 @@
 // export const API_URL = import.meta.env.VITE_API_URL;
-// export const API_URL = 'https://projetos.lsd.ufcg.edu.br/vera-ai';
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://projetos.lsd.ufcg.edu.br/vera-ai';
+// export const API_URL = 'http://localhost:8000';
